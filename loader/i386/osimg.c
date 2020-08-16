19b37206977493c67827a730b38753b4c795f20b
Connor Horman <chorman64@gmail.com>
2020-08-15 23:43:15 -0400

    Move GDT into assembly file

--- loader/i386/osimg.c
+++ loader/i386/osimg.c
@@ -16,25 +16,26 @@ extern const union ElfHeader __head;
 _Noreturn void _hlt(void);
 
 void load_osimg(void){
-    if((__head.e_ident[EI_MAG0]!=ELFMAG0)
-        ||(__head.e_ident[EI_MAG1]!=ELFMAG1)
-        ||(__head.e_ident[EI_MAG2]!=ELFMAG2)
-        ||(__head.e_ident[EI_MAG3]!=ELFMAG3)
-        ||(__head.e_ident[EI_DATA]!=ELFDATA2LSB)
-        ||(__head.e_ident[EI_VERSION]!=EV_CURRENT))
+    const union ElfHeader* head = &__head;
+    if((head->e_ident[EI_MAG0]!=ELFMAG0)
+        ||(head->e_ident[EI_MAG1]!=ELFMAG1)
+        ||(head->e_ident[EI_MAG2]!=ELFMAG2)
+        ||(head->e_ident[EI_MAG3]!=ELFMAG3)
+        ||(head->e_ident[EI_DATA]!=ELFDATA2LSB)
+        ||(head->e_ident[EI_VERSION]!=EV_CURRENT))
         _hlt();
     else{
-        if(__head.e_ident[EI_CLASS]==ELFCLASS64) {
-            if(__head.head64.e_machine!=EM_X86_64)
+        if(head->e_ident[EI_CLASS]==ELFCLASS64) {
+            if(head->head64.e_machine!=EM_X86_64)
                 _hlt();
-            Elf64_Addr entry = __head.head64.e_entry;
+            Elf64_Addr entry = head->head64.e_entry;
             if(entry>UINT32_MAX)
                 _hlt();
             void(*_start)(void) = (void(*)(void))entry;
-            Elf64_Off phead_off = __head.head64.e_phoff;
+            Elf64_Off phead_off = head->head64.e_phoff;
             if(phead_off>UINT32_MAX)
                 _hlt();
-            Elf64_Half phead_num = __head.head64.e_phnum;
+            Elf64_Half phead_num = head->head64.e_phnum;
             Elf64_Phdr* phead = (Elf64_Phdr *) ((unsigned char *) (&__head) + phead_off);
             for(Elf64_Phdr* chead = phead;chead!=phead+phead_num;chead++){
                 if(chead->p_type!=PT_LOAD)
