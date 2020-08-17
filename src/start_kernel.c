cd6fd9accdfa262c2fbfe63759f1a98f2b2f08a4
Connor Horman <chorman64@gmail.com>
2020-08-16 22:53:09 -0400

    Reach 64-bit mode

--- src/start_kernel.c
+++ src/start_kernel.c
@@ -2,11 +2,23 @@
 // Created by chorm on 2020-07-21.
 //
 
+#include <stdint.h>
+
+_Noreturn void _hlt(void);
+
 void printk(const char* v);
 
 _Noreturn __attribute__((section(".text.init"))) void start_kernel(void* multiboot){
     (void)multiboot;
-    printk("Hello World\n");
+    printk("Hello World");
+    _hlt();
+}
 
-    for(;;);
+__attribute__((section(".text.init"))) void handle_interrupt(void* v, uint64_t code,uint32_t errc){
+    switch(code){
+        case 9: // DF, _hlt for now
+            _hlt();
+        default:
+            ;
+    }
 }
