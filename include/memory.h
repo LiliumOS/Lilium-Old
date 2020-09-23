b9a7a0d2b5207bc9a7b9366d2e2cc77740b2dd9d
Connor Horman <chorman64@gmail.com>
2020-09-23 14:28:24 -0400

    add read_cr3 function (note: write_cr3 is not provided, to discourage direct writing to cr3. Store to interrupt frames instead)

--- include/memory.h
+++ include/memory.h
@@ -5,28 +5,37 @@
 #ifndef PHANTOMOS_MEMORY_H
 #define PHANTOMOS_MEMORY_H
 
+#include <stdint.h>
+#include <stddef.h>
+
+
 typedef struct PML5T{
-    uint64_t pml4t[512];
-}__attribute__((align(4096))) PML5T;
+    _Alignas(4096) int64_t pml4t[512];
+} PML5T;
 
 typedef struct PML4T{
-    uint64_t pdpt[512];
-}__attribute__((align(4096))) PML4T;
+    _Alignas(4096) int64_t pdpt[512];
+} PML4T;
 
 typedef struct PDPT{
-    uint64_t pdt[512];
-}__attribute__((align(4096))) PDPT;
+    _Alignas(4096) int64_t pdt[512];
+} PDPT;
 
-typedef struct PPT{
-    uint64_t pt[512];
-}__attribute__((align(4096))) PPT;
+typedef struct PDT{
+    _Alignas(4096) int64_t pt[512];
+} PPT;
 
 typedef struct PT{
-    uint64_t pe[512];
-}__attribute__((align(4096))) PT;
+    _Alignas(4096) int64_t pe[512];
+} PT;
 
-void* get_mapped_virtual_address(void* paddr);
+void* get_mapped_virtual_address(int64_t paddr);
 
-void* get_mapped_physical_addr(void* vaddr);
+int64_t get_mapped_physical_addr(void* vaddr);
+
+int64_t read_cr3();
+
+
+void* kmap(int64_t paddr,void* vaddr_hint,size_t pcount);
 
 #endif //PHANTOMOS_MEMORY_H
